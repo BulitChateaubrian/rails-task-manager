c54836e8b92eb94646a22e96ea7431db114ef58b
thibaultbrianceau <tbrianceau@gmail.com>
2022-03-22 12:47:03 +0100

    exo finis sans front et sans refracto

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params_task)
+
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+
+  private
+  def params_task
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
